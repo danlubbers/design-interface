d13b4a3958dd6ed165cc6c58fea213dc50ba886b
Dan Lubbers <lubbers.dan@gmail.com>
2019-05-29 13:06:41 -0400

    first commit

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+
+export default class Dashboard extends Component {
+  constructor(props) {
+    super(props) 
+    
+    this.state = {
+        click: false
+    }
+    this.handleClick = this.handleClick.bind(this);
+    
+  }
+
+  handleClick() {
+    this.setState({click: !this.state.click});
+  }
+
+
+    render () {
+      return (
+        <div className='dashboard-app'>
+          <div className='colors-section'>
+            <h1>Colors</h1>
+
+            <div className='show-hide-circle' onClick={_=> this.handleClick()}>
+            { this.state.click ? 
+              <div className='circle-before-container'>
+                <div className='circle-before'></div> 
+                <h2>radial-gradient(#999, #555, #111)</h2>
+              </div> : 
+              <div className='circle-after-container'>
+                <div className='circle-after'></div>
+                <h2>radial-gradient(#111, #555, #999)</h2>
+              </div>
+            }
+            </div>
+            
+         </div>
+        </div>
+      )
+    }
+}   
