0195bf48af4ed60f014f1c7a2f513659be97922f
Dan Lubbers <lubbers.dan@gmail.com>
2019-05-31 14:02:02 -0400

    Hex functionality is working and displays onClick now

--- src/Components/ColorPicker/ColorPicker.js
+++ src/Components/ColorPicker/ColorPicker.js
@@ -5,23 +5,27 @@ import { SketchPicker } from 'react-color';
 export default class ColorPicker extends Component {
     state = {
       displayColorPicker: false,
+      showHexButton: false,
       color1: {
         r: '245',
         g: '149',
         b: '35',
-        a: '87',
+        a: '1',
       },
       color2: {
         r: '245',
         g: '149',
         b: '35',
-        a: '87',
+        a: '1',
       },
     };
 
 
 handleClick = () => {
-    this.setState({ displayColorPicker: !this.state.displayColorPicker})
+    this.setState({ 
+        displayColorPicker: !this.state.displayColorPicker,
+        showHexButton: true
+    })
 };
 
 handleClose = () => {
@@ -32,7 +36,23 @@ handleChange = (color1) => {
     this.setState({ color1: color1.rgb })
 };
 
+handleToggle = () => {
+    this.setState({ showHexButton: true})
+};
+
     render() {  
+
+        // RGBA Values
+        const R = this.state.color1.r; 
+        const G = this.state.color1.g; 
+        const B = this.state.color1.b; 
+        const A = this.state.color1.a; 
+
+        // HEX Values
+        let hexR = `${R.toString(16).toUpperCase()}`;
+        let hexG = `${G.toString(16).toUpperCase()}`;
+        let hexB = `${B.toString(16).toUpperCase()}`;
+        let hexA = `${Math.round(A).toString(16).toUpperCase().slice(0, -1)}`;
 
         const styles = reactCSS({
             'default': {
@@ -41,7 +61,7 @@ handleChange = (color1) => {
                 height: '100px',
                 borderRadius: '50%',
                 border: '2px solid black',
-                background: `rgba(${ this.state.color1.r }, ${ this.state.color1.g }, ${ this.state.color1.b }, ${ this.state.color1.a })`,
+                background: `rgba(${ R }, ${ G }, ${ B }, ${ A })`,
               },
               swatch: {
                 padding: '5px',
@@ -67,11 +87,7 @@ handleChange = (color1) => {
             },
           });
 
-          let hexR = `${this.state.color1.r.toString(16).toUpperCase()}`;
-          let hexG = `${this.state.color1.g.toString(16).toUpperCase()}`;
-          let hexB = `${this.state.color1.b.toString(16).toUpperCase()}`;
-          let hexA = `${Math.round(this.state.color1.a).toString(16).toUpperCase().slice(0, -1)}`;
-
+        // Ternary to determine if conversion needs a '0' or not
           hexR.length === 1 ? hexR = `0${hexR}` : hexR = `${hexR}`;
           hexG.length === 1 ? hexG = `0${hexG}` : hexG = `${hexG}`;
           hexB.length === 1 ? hexB = `0${hexB}` : hexB = `${hexB}`;
@@ -82,9 +98,9 @@ handleChange = (color1) => {
         return(
           <div className='color-picker-app'>
             <div style={ styles.swatch } >
-              <div style={ styles.color } onClick={ this.handleClick }/> 
+              <div style={ styles.color } onClick={ this.handleClick } /> 
                 <h1>
-                    {`rgba(${ this.state.color1.r }, ${ this.state.color1.g }, ${ this.state.color1.b }, ${ this.state.color1.a })`} 
+                    {`rgba(${R}, ${G}, ${B}, ${A})`} 
                 </h1>
             </div>
             { this.state.displayColorPicker ? <div style={ styles.popover }>
@@ -92,9 +108,10 @@ handleChange = (color1) => {
               <SketchPicker color={ this.state.color1 } onChange={ this.handleChange } />
             </div> : null }
 
-          <div>
-            { `HEX Code: #${hexR}${hexG}${hexB}${hexA}` }
-          </div>
+
+            <div className='hex-code'>
+              { this.state.showHexButton ? `HEX Code: #${hexR}${hexG}${hexB}${hexA}` : null }
+            </div>
 
           </div>
         
